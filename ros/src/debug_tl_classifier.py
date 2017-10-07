9bd8b34b08f19e176aed25e050a23718ffd77770
Oleg Leyzerov <olegleyz@gmail.com>
2017-10-07 09:31:27 +0200

    remove unwanted files

--- ros/src/debug_tl_classifier.py
+++
@@ -1,9 +0,0 @@
-from tl_detector.light_classification.tl_classifier import *
-
-def main():
-	clsf = TLClassifier()
-	predict = clsf.get_classification('test_green1.jpg')
-	print (predict)
-
-if __name__ == '__main__':
-	main()
