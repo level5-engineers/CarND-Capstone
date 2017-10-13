2d4968a2898757ba07d8208c0d479ad8baa9a466
James Dunn <jwdunn1@gmail.com>
2017-10-12 21:46:30 -0700

    Add time stamp for current velocity
    Useful for rqt_plot

--- ros/src/styx/bridge.py
+++ ros/src/styx/bridge.py
@@ -103,6 +103,7 @@ class Bridge(object):
         tw = TwistStamped()
         tw.twist.linear.x = velocity
         tw.twist.angular.z = angular
+        tw.header.stamp = rospy.Time.now()
         return tw
 
     def create_steer(self, val):
